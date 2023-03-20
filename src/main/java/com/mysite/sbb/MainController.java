fbfa504f3f59eae3212d08cae1bccda5ac7ac829
mmunkyeong <jmk4379@naver.com>
2023-03-20 16:39:10 +0900

    MainController.java create

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,15 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // 스프링부트의 컨트롤러
+public class MainController {
+
+    @GetMapping("/sbb") // 요청된 URL과의 매핑을 담당
+    @ResponseBody
+    public String index() {
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+}
