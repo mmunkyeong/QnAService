b47790fbdc76150180d86182967f3d5f12cbdfe9
mmunkyeong <jmk4379@naver.com>
2023-03-21 11:12:40 +0900

    ROOT URL

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -12,4 +12,9 @@ public class MainController {
     public String index() {
         return "안녕하세요 sbb에 오신것을 환영합니다.";
     }
+
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list"; //완전히 새로운  URL로 요청
+    }
 }
